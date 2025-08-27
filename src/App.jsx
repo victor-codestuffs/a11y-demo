4ed82bfc20c0d46226f2b2bdbf310eadefbe1553
Victor Hoang <2126386+victor-codestuffs@users.noreply.github.com>
2025-08-26 21:01:44 -0700

    Add rs2

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {style} from '@react-spectrum/s2/style' with {type: 'macro'};
+import { Provider, Button } from '@react-spectrum/s2';
 import './App.css'
+import '@react-spectrum/s2/page.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Provider background="base">
+      <header style={{ height: "48px" }}>
+        hello
+      </header>
+      <main style={{ flex: 1 }}>
+        <Button
+          variant="accent"
+          onPress={() => alert('Hey there!')}>
+          Hello Spectrum 2!
+        </Button>
+      </main>
+    </Provider>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import macrosPlugin from 'unplugin-parcel-macros';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    macrosPlugin.vite(),
+  ],
   base: "/a11y-demo/",
 })
